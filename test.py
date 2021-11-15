37cbfa9c261e0f71a03c0a904954a50adb5bd43d
Cameron Johnson <cjohnson23@gwu.edu>
2021-11-14 21:04:25 -0500

    deleted

--- test.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-r = 4
-
-circleWidth = (2 * r) + 1
-[X, Y] = np.meshgrid(np.arange(1, (circleWidth + 1)), np.arange(1, (circleWidth + 1)))
-distFromCenter = np.sqrt(np.power(X - (r + 1), 2) + np.power(Y - (r + 1), 2))
-onPixels = np.abs(distFromCenter - r) < 0.5
-print(onPixels)
